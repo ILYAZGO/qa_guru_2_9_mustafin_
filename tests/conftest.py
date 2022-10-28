00557168bc5926910db7115a6ff88485d22447d6
ILYAZGO <mig_star@mail.ru>
2022-10-28 13:41:53 +0300

    requirements add

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ from selene.support.shared import browser
 def browser_preconfig():
     browser.config.browser_name="chrome"
     browser.config.base_url = 'https://demoqa.com'
-    #browser.config.window_width=1920
-    #browser.config.window_height=1080
+    browser.config.window_width=1920
+    browser.config.window_height=1080
     yield
